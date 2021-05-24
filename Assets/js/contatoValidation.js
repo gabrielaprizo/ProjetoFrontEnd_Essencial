18596c51e74027f8dd61eae200228413b6b0bd28
Gabriela Prizo <gabbprizo@gmail.com>
2021-05-24 13:09:29 -0300

    Html atualizações e Resolução de bug no JS

--- Assets/js/contatoValidation.js
+++ Assets/js/contatoValidation.js
@@ -2,14 +2,14 @@
   'use strict'
 
   // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  var forms = document.querySelectorAll('.needs-validation')
+  var forms = document.querySelectorAll('.needs-validation');
 
   // Loop over them and prevent submission
   Array.prototype.slice.call(forms)
     .forEach(function (form) {
       form.addEventListener('submit', function (event) {
         event.preventDefault()
-        event.stopPropagation()
+        event.stopPropagation();
 
         if (form.checkValidity()) {
             form.reset();
